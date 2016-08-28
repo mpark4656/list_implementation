28a6c9a77587922d06c263ea02a8e07608dcf0a2
mpark4656 <mpark4656@gmail.com>
2016-08-28 09:19:26 -0400

    Updated List.h
    Added reverse_iterator and const_reverse_iterator

--- src/List.h
+++ src/List.h
@@ -1,278 +1,365 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
-
-
-template <typename Object>
-class List
+namespace my
 {
-private:
-    struct Node
+    template <typename Object>
+    class List
     {
-        Object data;
-        Node* prev;
-        Node* next;
+    private:
+        struct Node
+        {
+            Object data;
+            Node* prev;
+            Node* next;
 
-        Node( const Object &d = Object{} , Node* p = nullptr , Node* n = nullptr )
-            : data{ d } , prev{ p } , next{ n } {}
+            Node( const Object &d = Object{} , Node* p = nullptr , Node* n = nullptr )
+                : data{ d } , prev{ p } , next{ n } {}
 
-        Node( Object &&d , Node* p = nullptr , Node* n = nullptr )
-            : data{ std::move(d) } , prev{ p } , next{ n } {}
-    };
-
-public:
-    class const_iterator
-    {
-    protected:
-        Node* current;
-
-        Object & retrieve() const
-        { return current->data; }
-
-        const_iterator( Node* p ): current{ p } {}
-
-        friend class List<Object>;
+            Node( Object &&d , Node* p = nullptr , Node* n = nullptr )
+                : data{ std::move(d) } , prev{ p } , next{ n } {}
+        };
 
     public:
-        const_iterator(): current{ nullptr } {}
-
-        const Object & operator* () const
-        { return retrieve(); }
-
-        const_iterator & operator++ ()
+        class const_iterator
         {
-            current = current->next;
-            return *this;
-        }
+        protected:
+            Node* current;
 
-        const_iterator operator++ ( int )
+            Object & retrieve() const
+            { return current->data; }
+
+            const_iterator( Node* p ): current{ p } {}
+
+            friend class List<Object>;
+
+        public:
+            const_iterator(): current{ nullptr } {}
+
+            const Object & operator* () const
+            { return retrieve(); }
+
+            const_iterator & operator++ ()
+            {
+                current = current->next;
+                return *this;
+            }
+
+            const_iterator operator++ ( int )
+            {
+                const_iterator old = *this;
+                ++( *this );
+                return old;
+            }
+
+            const_iterator operator-- ()
+            {
+                current = current->prev;
+                return *this;
+            }
+
+            const_iterator operator-- ( int )
+            {
+                const_iterator old = *this;
+                --( *this );
+                return old;
+            }
+
+            bool operator== ( const const_iterator &rhs ) const
+            { return current == rhs.current; }
+
+            bool operator!= ( const const_iterator &rhs ) const
+            { return !( *this == rhs ); }
+        };
+
+        class iterator: public const_iterator
         {
-            const_iterator old = *this;
-            ++( *this );
-            return old;
-        }
+        public:
+            iterator() {}
 
-        const_iterator operator-- ()
+            Object & operator* ()
+            { return const_iterator::retrieve(); }
+
+            const Object & operator* () const
+            { return const_iterator::operator*(); }
+
+            iterator & operator++ ()
+            {
+                this->current = this->current->next;
+                return *this;
+            }
+
+            iterator operator++ ( int )
+            {
+                iterator old = *this;
+                ++( *this );
+                return old;
+            }
+
+            iterator operator-- ()
+            {
+                this->current = this->current->prev;
+                return *this;
+            }
+
+            iterator operator-- ( int )
+            {
+                iterator old = *this;
+                --( *this );
+                return old;
+            }
+
+        protected:
+            iterator( Node* p ): const_iterator{ p } {}
+
+            friend class List<Object>;
+        };
+
+        class const_reverse_iterator: public const_iterator
         {
-            current = current->prev;
-            return *this;
-        }
+        public:
+            const_reverse_iterator() {}
 
-        const_iterator operator-- ( int )
+            const Object & operator* () const
+            { return const_iterator::retrieve(); }
+
+            const_reverse_iterator & operator++ ()
+            {
+                this->current = this->current->prev;
+                return *this;
+            }
+
+            const_reverse_iterator operator++ ( int )
+            {
+                const_reverse_iterator old = *this;
+                ++( *this );
+                return old;
+            }
+            const_reverse_iterator & operator-- ()
+            {
+                this->current = this->current->next;
+                return *this;
+            }
+
+            const_reverse_iterator operator-- ( int )
+            {
+                const_reverse_iterator old = *this;
+                --( *this );
+                return old;
+            }
+
+        protected:
+            const_reverse_iterator( Node* p ): const_iterator{ p } {}
+
+            friend class List<Object>;
+        };
+
+
+        class reverse_iterator: public const_reverse_iterator
         {
-            const_iterator old = *this;
-            --( *this );
-            return old;
-        }
+        public:
+            reverse_iterator() {}
 
-        bool operator== ( const const_iterator &rhs ) const
-        { return current == rhs.current; }
+            Object & operator* ()
+            { return const_reverse_iterator::retrieve(); }
 
-        bool operator!= ( const const_iterator &rhs ) const
-        { return !( *this == rhs ); }
-    };
+            const Object & operator* () const
+            { return const_reverse_iterator::operator*(); }
 
-    class iterator: public const_iterator
-    {
+            reverse_iterator & operator++ ()
+            {
+                this->current = this->current->prev;
+                return *this;
+            }
+
+            reverse_iterator operator++ ( int )
+            {
+                reverse_iterator old = *this;
+                ++( *this );
+                return old;
+            }
+
+            reverse_iterator operator-- ()
+            {
+                this->current = this->current->next;
+                return *this;
+            }
+
+            reverse_iterator operator-- ( int )
+            {
+                reverse_iterator old = *this;
+                --( *this );
+                return old;
+            }
+
+        protected:
+            reverse_iterator( Node* p ): const_reverse_iterator{ p } {}
+
+            friend class List<Object>;
+        };
+
     public:
-        iterator() {}
 
-        Object & operator* ()
-        { return const_iterator::retrieve(); }
+        List()
+        { init(); }
 
-        const Object & operator* () const
-        { return const_iterator::operator*(); }
-
-        iterator & operator++ ()
+        List( const List &rhs )
         {
-            this->current = this->current->next;
+            init();
+            for( auto &x : rhs ) {
+                push_back( x );
+            }
+        }
+
+        ~List()
+        {
+            clear();
+            delete head;
+            delete tail;
+        }
+
+        List & operator= ( const List &rhs )
+        {
+            List copy = rhs;
+            std::swap( *this , copy );
             return *this;
         }
 
-        iterator operator++ ( int )
+        List( List &&rhs ): theSize{ rhs.theSize } , head{ rhs.head } , tail{ rhs.tail }
         {
-            iterator old = *this;
-            ++( *this );
-            return old;
+            rhs.theSize = 0;
+            rhs.head = nullptr;
+            rhs.tail = nullptr;
         }
 
-    protected:
-        iterator( Node* p ): const_iterator{ p } {}
+        List & operator= ( List &&rhs )
+        {
+            std::swap( theSize , rhs.theSize );
+            std::swap( head , rhs.head );
+            std::swap( tail , rhs.tail );
 
-        friend class List<Object>;
+            return *this;
+        }
+
+        iterator begin()
+        { return iterator{ head->next }; }
+
+        const_iterator begin() const
+        { return const_iterator{ head->next }; }
+
+        iterator end()
+        { return iterator{ tail }; }
+
+        const_iterator end() const
+        { return const_iterator{ tail }; }
+
+        reverse_iterator rbegin()
+        { return reverse_iterator{ tail->prev }; }
+
+        const_reverse_iterator rbegin() const
+        { return const_reverse_iterator{ tail->prev }; }
+
+        reverse_iterator rend()
+        { return reverse_iterator{ head }; }
+
+        const_reverse_iterator rend() const
+        { return const_reverse_iterator{ head }; }
+
+
+
+        int size() const
+        { return theSize; }
+
+        bool empty() const
+        { return size() == 0 ;}
+
+        void clear()
+        {
+            while( !empty() )
+                pop_front();
+        }
+
+        Object & front()
+        { return *begin(); }
+
+        const Object & front() const
+        { return *begin(); }
+
+        Object & back()
+        { return *--end(); }
+
+        const Object & back() const
+        { return *--end(); }
+
+        void push_front( const Object &x )
+        { insert( begin() , x ); }
+
+        void push_front( Object &&x )
+        { insert( begin() , std::move( x ) ); }
+
+        void push_back( const Object &x )
+        { insert( end() , x ); }
+
+        void push_back( Object &&x )
+        { insert( end() , std::move( x ) ); }
+
+        void pop_front()
+        { erase( begin() ); }
+
+        void pop_back()
+        { erase( --end() ); }
+
+        iterator insert( iterator itr , const Object &x )
+        {
+            Node* p = itr.current;
+            theSize ++;
+
+            return { p->prev = p->prev->next = new Node{ x , p->prev , p } };
+        }
+
+        iterator insert( iterator itr , Object &&x )
+        {
+            Node* p = itr.current;
+            theSize ++;
+            return { p->prev = p->prev->next = new Node{ std::move( x ) , p->prev , p } };
+        }
+
+        iterator erase( iterator itr )
+        {
+            Node* p = itr.current;
+            iterator retVal{ p->next };
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+
+            delete p;
+
+            theSize --;
+
+            return retVal;
+        }
+
+        iterator erase( iterator from , iterator to )
+        {
+            for( iterator itr = from ; itr != to ; )
+                itr = erase( itr );
+
+            return to;
+        }
+
+    private:
+        int theSize;
+        Node *head;
+        Node *tail;
+
+        void init()
+        {
+            theSize = 0;
+            head = new Node;
+            tail = new Node;
+            head->next = tail;
+            tail->prev = head;
+        }
     };
-
-public:
-   /**
-    *   Default Constructor
-    */
-    List()
-    { init(); }
-
-   /**
-    *   Copy Constructor
-    */
-    List( const List &rhs )
-    {
-        init();
-        for( auto &x : rhs ) {
-            push_back( x );
-        }
-    }
-
-   /**
-    *   Destructor
-    */
-    ~List()
-    {
-        clear();
-        delete head;
-        delete tail;
-    }
-
-   /**
-    *   Copy Assignment
-    */
-    List & operator= ( const List &rhs )
-    {
-        List copy = rhs;
-        std::swap( *this , copy );
-        return *this;
-    }
-
-   /**
-    *   Move Constructor
-    */
-    List( List &&rhs ): theSize{ rhs.theSize } , head{ rhs.head } , tail{ rhs.tail }
-    {
-        rhs.theSize = 0;
-        rhs.head = nullptr;
-        rhs.tail = nullptr;
-    }
-
-   /**
-    *   Move Assignment
-    */
-    List & operator= ( List &&rhs )
-    {
-        std::swap( theSize , rhs.theSize );
-        std::swap( head , rhs.head );
-        std::swap( tail , rhs.tail );
-
-        return *this;
-    }
-
-
-    iterator begin()
-    { return iterator{ head->next }; }
-
-    const_iterator begin() const
-    { return const_iterator{ head->next }; }
-
-    iterator end()
-    { return iterator{ tail }; }
-
-    const_iterator end() const
-    { return const_iterator{ tail }; }
-
-
-    int size() const
-    { return theSize; }
-
-    bool empty() const
-    { return size() == 0 ;}
-
-
-   /**
-    *   Remove all elements in the list
-    */
-    void clear()
-    {
-        while( !empty() )
-            pop_front();
-    }
-
-    Object & front()
-    { return *begin(); }
-
-    const Object & front() const
-    { return *begin(); }
-
-    Object & back()
-    { return *--end(); }
-
-    const Object & back() const
-    { return *--end(); }
-
-    void push_front( const Object &x )
-    { insert( begin() , x ); }
-
-    void push_front( Object &&x )
-    { insert( begin() , std::move( x ) ); }
-
-    void push_back( const Object &x )
-    { insert( end() , x ); }
-
-    void push_back( Object &&x )
-    { insert( end() , std::move( x ) ); }
-
-    void pop_front()
-    { erase( begin() ); }
-
-    void pop_back()
-    { erase( --end() ); }
-
-    iterator insert( iterator itr , const Object &x )
-    {
-        Node* p = itr.current;
-        theSize ++;
-
-        return { p->prev = p->prev->next = new Node{ x , p->prev , p } };
-    }
-
-    iterator insert( iterator itr , Object &&x )
-    {
-        Node* p = itr.current;
-        theSize ++;
-        return { p->prev = p->prev->next = new Node{ std::move( x ) , p->prev , p } };
-    }
-
-    iterator erase( iterator itr )
-    {
-        Node* p = itr.current;
-        iterator retVal{ p->next };
-        p->prev->next = p->next;
-        p->next->prev = p->prev;
-
-        delete p;
-
-        theSize --;
-
-        return retVal;
-    }
-
-    iterator erase( iterator from , iterator to )
-    {
-        for( iterator itr = from ; itr != to ; )
-            itr = erase( itr );
-
-        return to;
-    }
-
-private:
-    int theSize;
-    Node *head;
-    Node *tail;
-
-    void init()
-    {
-        theSize = 0;
-        head = new Node;
-        tail = new Node;
-        head->next = tail;
-        tail->prev = head;
-    }
-};
-
+}
 
 #endif // LIST_H_INCLUDED
